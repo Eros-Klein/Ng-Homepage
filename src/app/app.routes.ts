02333dc0906050b08a8fff641254664c81d60b0a
Eros Klein <e.klein@HTBLALeonding.onmicrosoft.com>
2024-10-24 23:42:43 +0200

    create first component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'hello', pathMatch: 'full' },
+  { path: 'hello', component: HelloComponent }
+];
